5427a8c1e0aefc84a89652d4c61062d8474a1237
polycarpcc <134735512+polycarpcc@users.noreply.github.com>
2023-08-21 06:11:10 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 
-def get_fruityvice_data:
+def get_fruityvice_data():
   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
   # streamlit.text(fruityvice_response.json())
 
